41beacbaa6856dfaa4ee6f211b085cc624b6ef72
rahul72925 <rahulkhatri72925@gmail.com>
2022-11-18 20:52:51 +0530

    tweak: redirect home page on brand name click

--- component/header/index.js
+++ component/header/index.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
 import { StyledHeader } from "./styled";
 
 export const Header = () => {
+  const router = useRouter();
+  const handleBrandClick = () => {
+    router.push("/");
+  };
   return (
     <StyledHeader>
-      <a>Hacker News</a>
+      <a href="/" onClick={handleBrandClick}>
+        Hacker News
+      </a>
     </StyledHeader>
   );
 };
